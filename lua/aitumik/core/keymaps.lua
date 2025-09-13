638878ed49b01a2262373841ea2e90740de0a588
Nathan Kimutai <nathanpykimutai@gmail.com>
2025-09-13 11:01:51 +0300

    cursors is centered when moving through search matches and indentation works without reselection

--- lua/aitumik/core/keymaps.lua
+++ lua/aitumik/core/keymaps.lua
@@ -1,4 +1,4 @@
-local opts = { noremap = true,slient = true}
+local opts = { noremap = true,silent = true}
 
 vim.g.mapleader = " " 
 vim.g.maplocalleader = " " 
@@ -13,3 +13,20 @@ vim.keymap.set("v","J",":m '>+1<CR>gv=gv",{ desc = "moves lines down in the visu
 
 vim.keymap.set("n","<C-d>","<C-d>zz",{ desc = "move down in buffer with cursor centered"})
 vim.keymap.set("n","<C-u>","<C-u>zz",{ desc = "move up in buffer with cursor centered"})
+
+vim.keymap.set("n","n","nzzzv")
+vim.keymap.set("n","N","Nzzzv")
+
+-- should be able to use a dot here to repeat last command, not working now
+vim.keymap.set("v","<","<gv",opts)
+vim.keymap.set("v",">",">gv",opts)
+
+-- paste without replacing clipboard content
+vim.keymap.set("x","<leader>p",[["_dP]])
+
+vim.keymap.set("v","p",'"_dp',opts)
+
+vim.keymap.set({ "n", "v" },"<leader>d",[["_d]])
+
+-- Dangling highlights are cleared
+vim.keymap.set("n","<C-c>",":nohls<CR>",{desc = "clears search highlights"})
