788e1eb4afe4f44826e27b08e542aecf2b649574
Nathan Kimutai <nathanpykimutai@gmail.com>
2025-09-16 10:15:41 +0300

    add oil file explorer and telescope, now i can use it as an editor

---
+++ lua/aitumik/plugins/telescope.lua
@@ -0,0 +1,29 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  cmd = "Telescope",
+  version = false,
+  dependencies = {
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+    },
+  },
+  keys = {
+    { "<leader>ff", function() require("telescope.builtin").find_files() end, desc = "Find files" },
+    { "<leader>fg", function() require("telescope.builtin").live_grep() end,  desc = "Live grep" },
+    { "<leader>fb", function() require("telescope.builtin").buffers() end,    desc = "Buffers" },
+    { "<leader>fh", function() require("telescope.builtin").help_tags() end, desc = "Help tags" },
+  },
+  config = function()
+    local telescope = require("telescope")
+
+    telescope.setup({
+      defaults = {
+        path_display = { "smart" },
+      },
+    })
+
+    -- load fzf extension if available
+    pcall(telescope.load_extension, "fzf")
+  end,
+}
